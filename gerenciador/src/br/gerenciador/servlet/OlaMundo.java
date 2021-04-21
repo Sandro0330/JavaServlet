1db985afbf6989d5f8ec716dc1e322939b1a1e34
Sandro0330 <sandro030486@gmail.com>
2021-04-21 15:15:23 -0300

    Configurando e testando JavaServlet com Tomcat

---
+++ gerenciador/src/br/gerenciador/servlet/OlaMundo.java
@@ -0,0 +1,25 @@
+package br.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OlaMundo extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Olá mundo: Java Servlet ");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+}
