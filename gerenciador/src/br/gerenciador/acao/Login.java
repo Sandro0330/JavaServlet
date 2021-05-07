01be1d8462eb9938a2e0da8a9aacf0fd92bf1430
Sandro0330 <sandro030486@gmail.com>
2021-05-07 20:52:34 -0300

    Verificando Login

--- gerenciador/src/br/gerenciador/acao/Login.java
+++ gerenciador/src/br/gerenciador/acao/Login.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.gerenciador.modelo.Banco;
+import br.gerenciador.modelo.Usuario;
+
 public class Login implements Acao {
 
 	@Override
@@ -16,7 +19,17 @@ public class Login implements Acao {
 		
 		System.out.println("Logando " + login);
 		
-		return "redirect:entrada?acao=ListaEmpresas";
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(login, senha);
+		
+		if(usuario != null) {
+			System.out.println("Usuário existe");
+			return "redirect:entrada?acao=ListaEmpresas";
+		} else {
+			System.out.println("Usuário não cadastrado");
+			return "redirect:entrada?acao=LoginForm";
+		}
+		
 	}
 
 }
