562f7f97248ed2aa3aa290ccced70c3b53240945
Sandro0330 <sandro030486@gmail.com>
2021-05-13 17:13:22 -0300

    Escolhendo o modo  de retorno entre JSON ou XML

--- gerenciador/src/br/gerenciador/servlet/EmpresaService.java
+++ gerenciador/src/br/gerenciador/servlet/EmpresaService.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.gerenciador.modelo.Banco;
 import br.gerenciador.modelo.Empresa;
@@ -24,12 +25,30 @@ public class EmpresaService extends HttpServlet {
    
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Empresa> empresas = new Banco().getEmpresas();
-	
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
 		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+		String valor = request.getHeader("Accept");
+		
+		if(valor.contains("xml")) {
+			
+			XStream xStream = new XStream(); 
+			xStream.alias("empresa", Empresa.class);
+			String xml = xStream.toXML(empresas);
+			
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+			
+		} else if(valor.contains("json")) {
+			
+			//Retorna uma lista JSON
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+			
+		} else {
+			response.getWriter().print("{'message': 'no content'}");
+		}
 		
 	}
 
