9ed43ca4bf77aac6a5635241c881f2eff7f14037
Sandro0330 <sandro030486@gmail.com>
2021-05-21 16:43:14 -0300

    Craindo a classe Repository e salvando o primeiro dado na tabela

--- spring-data/src/main/java/br/com/spring/data/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/spring/data/SpringDataApplication.java
@@ -1,13 +1,32 @@
 package br.com.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.spring.data.orm.Cargo;
+import br.com.spring.data.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+	private final CargoRepository cargoRepository;
+
+	
+	public SpringDataApplication(CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("Desenvolvedor de Software");
+		
+		cargoRepository.save(cargo);
 	}
 
 }
